b2d98f9b6edd88e3977d27afddf049cf49c23812
resie001 <aderesie@zohomail.com>
2021-01-18 23:59:14 +0700

    Menambahkan logic pads view

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func btnAskClicked(_ sender: UIButton) {
+        self.imageView.image = self.ballArray.randomElement()
+    }
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
